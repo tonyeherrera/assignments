392932a0959086d1948de472a83421067770e7ef
tonyeherrera <74445648+tonyeherrera@users.noreply.github.com>
2022-03-27 18:59:01 -0400

    initial commit

---
+++ exercises/js-practice/js-redacted/app.js
@@ -0,0 +1,8 @@
+var thing = document.getElementsByClassName("thing");
+ 
+for ( let i = 0; i < thing.length; i++)
+thing[i].textContent = "**Redacted**"
+console.log("hello")
+
+var thingQuery = document.querySelector(".thing")
+    console.log(thingQuery)

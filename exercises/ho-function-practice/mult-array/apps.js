9adab4dc7e1b78d77a19ed01188c56a8058b6b93
tonyeherrera <74445648+tonyeherrera@users.noreply.github.com>
2022-05-08 15:50:03 -0400

    multiple method array

---
+++ exercises/ho-function-practice/mult-array/apps.js
@@ -0,0 +1,75 @@
+// You should use multiple array methods to solve these problems. Don't use `for` loops!
+
+// Using the provided `peopleArray` (bottom of this article), write a function that:
+
+// 1. Returns a list of everyone older than 18, which is
+// 2. sorted alphabetically by last name, and where
+// 3. each name and age is embedded in a string that looks like an HTML `<li>` element.
+
+
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+function sortedOfAge(arr){
+     const filtered = arr.filter(person => person.age > 18);
+     const sorted = filtered.sort((a,b) => {
+        if(a.lastName[0] < b.lastName[0]){
+            return -1
+        }else if (a.lastName[0] > b.lastName[0]){
+            return 1
+        }else{
+            return 0
+        }
+    });
+    return sorted.map(person => {return "<li>" + person.firstName + " " + person.lastName + " is "+ person.age + "</li>"})
+    
+    
+ }
+ 
+ console.log(sortedOfAge(peopleArray));
+ 
+ /*
+ Output:
+ [
+     "<li>Kyle Mooney is 27</li>",
+     "<li>Sarah Palin is 47</li>",
+     "<li>Rick Sanchez is 78</li>",
+     "<li>Morty Smith is 29</li>",
+     "<li>Lev Tolstoy is 82</li>"
+ ]
+ */
